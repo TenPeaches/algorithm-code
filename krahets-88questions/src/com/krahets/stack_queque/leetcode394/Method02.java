07a1fa54222eeef395623df791bb91002ceaff4f
TenPeaches <2060853750@qq.com>
2024-10-22 15:54:55 +0800

    新增leetcode394题法二递归

--- krahets-88questions/src/com/krahets/stack_queque/leetcode394/Method02.java
+++ krahets-88questions/src/com/krahets/stack_queque/leetcode394/Method02.java
@@ -3,7 +3,32 @@ package com.krahets.stack_queque.leetcode394;
 import java.util.Stack;
 
 public class Method02 {
-    public static void main(String[] args) {
-        System.out.println("hello world");
+    public String decodeString(String s) {
+        return dfs(s, 0)[0];
     }
+
+    private String[] dfs(String s, int index) {
+        StringBuilder sb = new StringBuilder();
+        int beishu = 0;
+        while (index < s.length()) {
+            char c = s.charAt(index);
+            if (c >= '0' && c <= '9') {
+                beishu = beishu * 10 + Integer.parseInt(String.valueOf(c));
+            } else if (c == '[') {
+                String temp[] =dfs(s, index + 1);
+                index = Integer.parseInt(temp[1]);
+                while (beishu > 0){
+                    sb.append(temp[0]);
+                    beishu--;
+                }
+            } else if (c == ']') {
+                    return new String[]{sb.toString(), String.valueOf(index)};
+            } else {
+                sb.append(c);
+            }
+            index++;
+        }
+        return new String[]{sb.toString()};
+    }
+
 }
