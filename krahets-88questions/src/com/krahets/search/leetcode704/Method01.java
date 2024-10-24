cb4f124c3a44b50393cde8ef8e9f0646d9190e50
TenPeaches <2060853750@qq.com>
2024-10-24 14:15:30 +0800

    新增leetcode704题法一：二分查找

---
+++ krahets-88questions/src/com/krahets/search/leetcode704/Method01.java
@@ -0,0 +1,19 @@
+package com.krahets.search.leetcode704;
+
+public class Method01 {
+    public int search(int[] nums, int target) {
+        int len = nums.length;
+        int left = 0, right = len - 1;
+        while (left <= right) {
+            int mid=left+(right-left)/2;
+            if(nums[mid]>target){
+                right=mid-1;
+            }else if(nums[mid]<target){
+                left=mid+1;
+            }else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
