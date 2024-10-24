38d6e83f1f51744f7864208dd12439645c96f84e
TenPeaches <2060853750@qq.com>
2024-10-24 15:09:47 +0800

    新增leetcode278题法一：二分查找

---
+++ krahets-88questions/src/com/krahets/search/leetcode278/Method01.java
@@ -0,0 +1,34 @@
+package com.krahets.search.leetcode278;
+
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+public class Method01 extends VersionControl {
+
+    public int firstBadVersion(int n) {
+        int left = 1; // 左边界初始化为 1，表示从第 1 个版本开始
+        int right = n; // 右边界初始化为 n，表示到第 n 个版本为止
+
+        // 二分查找，直到 left 和 right 相遇
+        while (left < right) {
+            int mid = left + (right - left) / 2; // 计算中间版本
+
+            // 如果 mid 是坏的版本
+            if (isBadVersion(mid)) {
+                right = mid; // 则第一个坏版本在 mid 左侧（包括 mid）
+            } else {
+                left = mid + 1; // 否则第一个坏版本在 mid 的右侧
+            }
+        }
+
+        // 当结束时，left 和 right 相等，指向第一个坏版本
+        return left; // 返回第一个坏版本的版本号
+    }
+}
+
+class VersionControl {
+    public boolean isBadVersion(int version) {
+        // 这里实现具体的判断是否是坏版本的方法,这里做题的环境有，就不写了。
+        return false;
+    }
+}
+
--- krahets-88questions/src/com/krahets/search/leetcode704/Method01.java
+++ krahets-88questions/src/com/krahets/search/leetcode704/Method01.java
@@ -1,19 +1,37 @@
 package com.krahets.search.leetcode704;
 
 public class Method01 {
+    /**
+     * 在有序数组中使用二分查找法查找目标值
+     * @param nums 有序数组
+     * @param target 需要查找的目标值
+     * @return 目标值在数组中的索引，如果不存在则返回 -1
+     */
     public int search(int[] nums, int target) {
-        int len = nums.length;
-        int left = 0, right = len - 1;
+        int len = nums.length; // 数组的长度
+        int left = 0; // 左边界初始化为数组的起始索引
+        int right = len - 1; // 右边界初始化为数组的末尾索引
+
+        // 当左边界小于等于右边界时，继续进行查找
         while (left <= right) {
-            int mid=left+(right-left)/2;
-            if(nums[mid]>target){
-                right=mid-1;
-            }else if(nums[mid]<target){
-                left=mid+1;
-            }else {
-                return mid;
+            int mid = left + (right - left) / 2; // 计算中间索引
+
+            // 如果中间值大于目标值，说明目标值在左半部分
+            if (nums[mid] > target) {
+                right = mid - 1; // 更新右边界
+            }
+            // 如果中间值小于目标值，说明目标值在右半部分
+            else if (nums[mid] < target) {
+                left = mid + 1; // 更新左边界
+            }
+            // 如果中间值等于目标值，返回中间索引
+            else {
+                return mid; // 找到目标值，返回其索引
             }
         }
+
+        // 如果目标值不存在于数组中，返回 -1
         return -1;
     }
 }
+
