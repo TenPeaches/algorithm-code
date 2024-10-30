dceefd59edfd4380a72e5c4a1ac552922223cc1d
TenPeaches <2060853750@qq.com>
2024-10-30 19:50:21 +0800

    新增leetcode122题法一：贪心,leetcode240题法一：Z字形查找

---
+++ krahets-88questions/src/com/krahets/greedy/leetcode240/Method01.java
@@ -0,0 +1,17 @@
+package com.krahets.greedy.leetcode240;
+
+public class Method01 {
+        public boolean searchMatrix(int[][] matrix, int target) {
+            int i=matrix.length-1,j=0;
+            while (i>=0&&j<matrix[0].length){
+                if(matrix[i][j]<target)
+                    j++;
+                else if(matrix[i][j]>target) {
+                    i--;
+                }
+                else
+                    return true;
+            }
+            return false;
+        }
+    }
