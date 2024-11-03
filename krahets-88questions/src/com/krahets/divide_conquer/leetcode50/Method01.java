dc9225f13a3543f88d0197344993531b321d7307
TenPeaches <2060853750@qq.com>
2024-11-03 10:55:39 +0800

    新增leetcode50题法一：快速幂+递归

--- krahets-88questions/src/com/krahets/divide_conquer/leetcode50/Method01.java
+++ krahets-88questions/src/com/krahets/divide_conquer/leetcode50/Method01.java
@@ -2,14 +2,15 @@ package com.krahets.divide_conquer.leetcode50;
 
 public class Method01 {
     public double myPow(double x, int n) {
-        return n >= 0? quickMul(x, n) : 1.0 / quickMul(x, -n);
+        return n >= 0 ? quickMul(x, n) : 1.0 / quickMul(x, -n);
     }
 
     private double quickMul(double x, int n) {
-        if(n == 0){
-            return 1;
+        if (n == 0) {
+            return 1; // 任何数的0次幂都是1
         }
-        double y =quickMul(x, n/2);
-        return n % 2 == 0? y * y : y * y * x;
+        double y = quickMul(x, n / 2); // 递归计算x的n/2次幂
+        return n % 2 == 0 ? y * y : y * y * x; // 判定n是偶数还是奇数
     }
 }
+
