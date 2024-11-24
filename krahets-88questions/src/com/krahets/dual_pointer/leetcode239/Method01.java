44e8da0112113e215c49938c9fb5604437946d7b
TenPeaches <2060853750@qq.com>
2024-11-24 14:13:20 +0800

    新增leetcode239题法一：单调队列

--- krahets-88questions/src/com/krahets/dual_pointer/leetcode239/Method01.java
+++ krahets-88questions/src/com/krahets/dual_pointer/leetcode239/Method01.java
@@ -5,25 +5,41 @@ import java.util.LinkedList;
 
 public class Method01 {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        if(nums.length == 0 || k == 0) return new int[0];
+        // 如果输入数组为空或窗口大小为0，则返回空数组
+        if (nums.length == 0 || k == 0) return new int[0];
+
+        // 使用双端队列来存储当前窗口的元素
         Deque<Integer> deque = new LinkedList<>();
+        // 结果数组，长度为输入数组长度 - 窗口大小 + 1
         int[] res = new int[nums.length - k + 1];
-        // 未形成窗口
-        for(int i = 0; i < k; i++) {
-            while(!deque.isEmpty() && deque.peekLast() < nums[i])
-                deque.removeLast();
+
+        // 未形成完整窗口的情况，填充前 k 个元素
+        for (int i = 0; i < k; i++) {
+            // 移除队列中比当前元素小的所有元素
+            while (!deque.isEmpty() && deque.peekLast() < nums[i]) {
+                deque.removeLast(); // 去掉队尾元素
+            }
+            // 将当前元素添加到队列的尾部
             deque.addLast(nums[i]);
         }
+        // 将队列的首部元素（当前窗口的最大值）存入结果数组
         res[0] = deque.peekFirst();
-        // 形成窗口后
-        for(int i = k; i < nums.length; i++) {
-            if(deque.peekFirst() == nums[i - k])
+
+        // 从第 k 个元素开始，开始滑动窗口
+        for (int i = k; i < nums.length; i++) {
+            // 如果队列的首部元素是滑出窗口的元素，则将其移除
+            if (deque.peekFirst() == nums[i - k]) {
                 deque.removeFirst();
-            while(!deque.isEmpty() && deque.peekLast() < nums[i])
-                deque.removeLast();
+            }
+            // 移除队列中所有比当前元素小的元素
+            while (!deque.isEmpty() && deque.peekLast() < nums[i]) {
+                deque.removeLast(); // 去掉队尾元素
+            }
+            // 将当前元素添加到队列的尾部
             deque.addLast(nums[i]);
+            // 将队列的首部元素（当前窗口的最大值）存入结果数组
             res[i - k + 1] = deque.peekFirst();
         }
-        return res;
+        return res; // 返回记录最大值的数组
     }
 }
