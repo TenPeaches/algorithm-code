fb3d488654e4f56533c057d3c2f9506979cdb313
TenPeaches <2060853750@qq.com>
2024-12-02 11:16:30 +0800

    新增leetcode231题法一：二进制的表示的注释

--- krahets-88questions/src/com/krahets/bit_operation/leetcode231/Method01.java
+++ krahets-88questions/src/com/krahets/bit_operation/leetcode231/Method01.java
@@ -1,10 +1,14 @@
 package com.krahets.bit_operation.leetcode231;
 
 public class Method01 {
+
     public boolean isPowerOfTwo(int n) {
+        // 检查 n 是否小于或等于 0， 如果是则不是 2 的幂
         if (n <= 0) {
             return false;
         }
+        // 检查 n 是否为 2 的幂，使用位与运算：2 的幂具有唯一一个 '1' 位
         return (n & (n - 1)) == 0;
     }
 }
+
