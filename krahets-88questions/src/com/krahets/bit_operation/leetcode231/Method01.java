c2969d5a5bb261f7cd3fa1a8bbd2dddabff96ca0
TenPeaches <2060853750@qq.com>
2024-12-02 11:15:55 +0800

    新增leetcode231题法一：二进制的表示

---
+++ krahets-88questions/src/com/krahets/bit_operation/leetcode231/Method01.java
@@ -0,0 +1,10 @@
+package com.krahets.bit_operation.leetcode231;
+
+public class Method01 {
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        return (n & (n - 1)) == 0;
+    }
+}
