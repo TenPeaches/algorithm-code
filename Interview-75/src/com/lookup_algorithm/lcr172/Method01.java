21a67d3f225e166348ab98d1ffed9cfb4af9dfcd
TenPeaches <2060853750@qq.com>
2025-03-04 23:16:54 +0800

    新增剑指offer120. 寻找文件副本,法三：原地交换

---
+++ Interview-75/src/com/lookup_algorithm/lcr172/Method01.java
@@ -0,0 +1,37 @@
+package com.lookup_algorithm.lcr172;
+
+public class Method01 {
+    public int countTarget(int[] scores, int target) {
+        if(scores==null ||scores.length==0){
+            return 0;
+        }
+        int i=0, j=scores.length-1, count=0;
+        while (i<j){
+             int mid = (i+j)/2;
+             if(scores[mid]>target){
+                 j=mid-1;
+             }else if(scores[mid]< target){
+                 i=mid+1;
+             }else{
+                 i=mid;
+                 break;
+             }
+        }
+        if(scores[i]!=target){
+            return 0;
+        }
+        for (int k = i; k <scores.length ; k++) {
+            if(scores[k]!=target){
+                break;
+            }
+            count++;
+        }
+        for (int k = i; k >=0 ; k--) {
+            if(scores[k]!=target){
+                break;
+            }
+            count++;
+        }
+        return count-1;
+    }
+}
