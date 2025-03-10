e2f570f2dde41a85d1525ea32f313f339bcd25a1
TenPeaches <2060853750@qq.com>
2025-03-10 19:56:45 +0800

    新增剑指offer144. 翻转二叉树,法一：辅助队列

---
+++ Interview-75/src/com/search_backtracking/lcr144/Method02.java
@@ -0,0 +1,29 @@
+package com.search_backtracking.lcr144;
+
+import com.search_backtracking.lcr149.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Method02 {
+    public TreeNode flipTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            TreeNode temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+        return root;
+    }
+}
