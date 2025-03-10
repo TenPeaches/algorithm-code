380134e1357dc9e3fc7f9a72d4ec3c61685ae7b3
TenPeaches <2060853750@qq.com>
2025-03-10 20:01:56 +0800

    新增剑指offer145. 判断对称二叉树,法一：递归

---
+++ Interview-75/src/com/search_backtracking/Method01.java
@@ -0,0 +1,25 @@
+package com.search_backtracking;
+
+import com.search_backtracking.lcr149.TreeNode;
+
+public class Method01 {
+    public boolean checkSymmetricTree(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
+}
