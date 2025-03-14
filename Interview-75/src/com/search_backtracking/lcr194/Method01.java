cb68f24abe10d8399f676af831a62370bc06f5c6
TenPeaches <2060853750@qq.com>
2025-03-14 21:00:07 +0800

    新增剑指offer194. 寻找二叉树的最近公共祖先,法一：深度优先搜索

---
+++ Interview-75/src/com/search_backtracking/lcr194/Method01.java
@@ -0,0 +1,17 @@
+package com.search_backtracking.lcr194;
+
+import com.search_backtracking.lcr149.TreeNode;
+
+public class Method01 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        }
+        return left != null ? left : right;
+    }
+}
