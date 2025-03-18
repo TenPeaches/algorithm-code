af585b2eea9cba561b394925cc73c9890c4b9b26
TenPeaches <2060853750@qq.com>
2025-03-18 22:34:19 +0800

    新增剑指offer152. 验证二叉搜索树的后序遍历序列,法二：辅助单调栈

--- Interview-75/src/com/divide_algorithm/lcr152/Method02.java
+++ Interview-75/src/com/divide_algorithm/lcr152/Method02.java
@@ -1,4 +1,18 @@
 package com.divide_algorithm.lcr152;
 
+import java.util.Stack;
+
 public class Method02 {
+    public boolean verifyTreeOrder(int[] postorder) {
+        Stack<Integer> stack = new Stack<>();
+        int root = Integer.MAX_VALUE;
+        for (int i = postorder.length - 1; i >= 0; i--) {
+            if (postorder[i] > root) return false;
+            while (!stack.isEmpty() && stack.peek() < postorder[i]) {
+                root = stack.pop();
+            }
+            stack.push(postorder[i]);
+        }
+        return true;
+    }
 }
