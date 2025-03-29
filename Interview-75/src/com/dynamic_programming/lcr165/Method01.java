7642f111ebfaa0ffa5703dee628ba80e8f4adca8
TenPeaches <2060853750@qq.com>
2025-03-29 22:58:18 +0800

    新增剑指offer165. 解密数字,法一：字符串遍历

---
+++ Interview-75/src/com/dynamic_programming/lcr165/Method01.java
@@ -0,0 +1,12 @@
+package com.dynamic_programming.lcr165;
+
+public class Method01 {
+    public int crackNumber(int ciphertext) {
+        String s=ciphertext+"";
+        int a=1,b=1;
+        for (int i = 0; i < s.length()-1; i++) {
+            String c=s.substring(i,i+2);
+        }
+        return 0;
+    }
+}
