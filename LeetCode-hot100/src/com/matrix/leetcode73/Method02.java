26e84f43e2e25f85652d792af078183b9775e136
TenPeaches <2060853750@qq.com>
2025-05-27 09:31:46 +0800

    新增leetcod73.矩阵置零,法一：使用一个标记变量

---
+++ LeetCode-hot100/src/com/matrix/leetcode73/Method02.java
@@ -0,0 +1,25 @@
+package com.matrix.leetcode73;
+
+public class Method02 {
+    public void setZeroes(int[][] matrix) {
+        boolean col0_flag = false;
+        int row = matrix.length;
+        int col = matrix[0].length;
+        for (int i = 0; i < row; i++) {
+            if (matrix[i][0] == 0) col0_flag = true;
+            for (int j = 1; j < col; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = row - 1; i >= 0; i--) {
+            for (int j = col - 1; j >= 1; j--) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+            if (col0_flag) matrix[i][0] = 0;
+        }
+    }
+}
