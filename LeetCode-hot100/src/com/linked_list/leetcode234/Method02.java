ce2e76cd2a77242bbc183467f84850978f6ad9b4
TenPeaches <2060853750@qq.com>
2025-06-03 12:52:21 +0800

    新增leetcod234.回文链表,法二：寻找中间节点+反转链表

---
+++ LeetCode-hot100/src/com/linked_list/leetcode234/Method02.java
@@ -0,0 +1,39 @@
+package com.linked_list.leetcode234;
+
+public class Method02 {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        ListNode middle = middleNode(head);
+        ListNode reverse = reverseList(middle);
+        while (reverse != null) {
+            if (head.val != reverse.val) {
+                return false;
+            }
+            head = head.next;
+            reverse = reverse.next;
+        }
+        return true;
+    }
+    private ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
