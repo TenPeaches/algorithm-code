d4d30176bb7f317fde65779c8b0c3b89935265f7
TenPeaches <2060853750@qq.com>
2025-06-03 13:06:33 +0800

    新增leetcod141.环形链表,法一：快慢指针

--- LeetCode-hot100/src/com/linked_list/leetcode234/ListNode.java
+++ LeetCode-hot100/src/com/linked_list/leetcode234/ListNode.java
@@ -1,9 +1,9 @@
 package com.linked_list.leetcode234;
 
 public class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    public int val;
+    public ListNode next;
+    public ListNode() {}
+    public ListNode(int val) { this.val = val; }
+    public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
