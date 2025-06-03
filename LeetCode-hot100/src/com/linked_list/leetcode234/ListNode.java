a8d00742765b7855ac7113398904f17ee8bcb8e5
TenPeaches <2060853750@qq.com>
2025-06-03 12:22:10 +0800

    新增leetcod234.回文链表,法一：将值复制到数组中后用双指针法

---
+++ LeetCode-hot100/src/com/linked_list/leetcode234/ListNode.java
@@ -0,0 +1,9 @@
+package com.linked_list.leetcode234;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
---
+++ LeetCode-hot100/src/com/linked_list/leetcode234/Method01.java
@@ -0,0 +1,7 @@
+package com.linked_list.leetcode234;
+
+public class Method01 {
+    public boolean isPalindrome(ListNode head) {
+
+    }
+}
