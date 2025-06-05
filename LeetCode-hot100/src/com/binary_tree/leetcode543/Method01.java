2322c3b57679e836f93fcf1f69036974e7dcfca2
TenPeaches <2060853750@qq.com>
2025-06-05 20:52:00 +0800

    移动包位置

--- LeetCode-hot100/src/com/binary_tree/leetcode543/Method01.java
+++ LeetCode-hot100/src/com/binary_tree/leetcode543/Method01.java
@@ -1,6 +1,6 @@
 package com.binary_tree.leetcode543;
 
-import com.search_backtracking.lcr149.TreeNode;
+import com.binary_tree.leetcode94.TreeNode;
 
 public class Method01 {
     public int diameterOfBinaryTree(TreeNode root) {
