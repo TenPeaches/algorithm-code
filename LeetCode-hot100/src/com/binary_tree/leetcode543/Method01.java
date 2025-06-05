73513d512a391cf12d1a7580663b95887c58f3e8
TenPeaches <2060853750@qq.com>
2025-06-05 20:56:49 +0800

    新增leetcod543.二叉树的直径,法一：深度优先搜索

--- LeetCode-hot100/src/com/binary_tree/leetcode543/Method01.java
+++ LeetCode-hot100/src/com/binary_tree/leetcode543/Method01.java
@@ -3,7 +3,19 @@ package com.binary_tree.leetcode543;
 import com.binary_tree.leetcode94.TreeNode;
 
 public class Method01 {
+    int max = 0;
     public int diameterOfBinaryTree(TreeNode root) {
+        dpth(root);
+        return max;
+    }
 
+    private int dpth(TreeNode root) {
+        if (root == null) {
+            return -1;
+        }
+        int left = dpth(root.left)+1;
+        int right = dpth(root.right)+1;
+        max = Math.max(max, left+right);
+        return Math.max(left, right);
     }
 }
