7f15f88cc59f506b70b9823c97058b03b3a91e0a
TenPeaches <2060853750@qq.com>
2025-11-03 11:41:07 +0800

    重做leetcode49.字母异位词分组,法一：排序

--- LeetCode-hot100/src/com/hash/leetcode49/Method01.java
+++ LeetCode-hot100/src/com/hash/leetcode49/Method01.java
@@ -4,19 +4,15 @@ import java.util.*;
 
 public class Method01 {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> map=new HashMap<>();
+        Map<String, List<String>> map = new HashMap<>();
         for (int i = 0; i < strs.length; i++) {
-            String s=strs[i];
-            char[] c=s.toCharArray();
-            Arrays.sort(c);
-            String str=String.valueOf(c);
-            if(map.containsKey(str)) {
-                map.get(str).add(s);
-            }else {
-                List<String> list=new ArrayList<>();
-                list.add(s);
-                map.put(str, list);
+            char[] chars = strs[i].toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
             }
+            map.get(key).add(strs[i]);
         }
         return new ArrayList<List<String>>(map.values());
     }
