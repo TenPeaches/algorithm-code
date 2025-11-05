446c0c672a3934620fcf3bf4c6823f5899757d3a
TenPeaches <2060853750@qq.com>
2025-11-05 19:10:51 +0800

    leetcode283.移动零,法一：双指针

--- LeetCode-hot100/src/com/double_pointer/leetcode283/Method01.java
+++ LeetCode-hot100/src/com/double_pointer/leetcode283/Method01.java
@@ -2,14 +2,18 @@ package com.double_pointer.leetcode283;
 
 public class Method01 {
     public void moveZeroes(int[] nums) {
-        int i = 0;
-        for (int j = 0; j < nums.length; j++) {
-            if(nums[j]!= 0){
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
+        int i = 0, j = 0;
+        while (j < nums.length){
+            if (nums[j]!= 0){
+                swap(nums, i, j);
                 i++;
             }
+            j++;
         }
+    }
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
