737150df1b26620aa6231f24c5e5b26f9d8eb4b3
TenPeaches <2060853750@qq.com>
2025-11-10 11:27:27 +0800

    leetcode560. 和为K的子数组,法一：前缀和 + 哈希表

--- LeetCode-hot100/src/com/substring/leetcode560/Method01.java
+++ LeetCode-hot100/src/com/substring/leetcode560/Method01.java
@@ -5,20 +5,15 @@ import java.util.Map;
 
 public class Method01 {
     public int subarraySum(int[] nums, int k) {
-        int len = nums.length;
-        int[] sum = new int[len + 1];
-        sum[0] = 0;
-        for (int i = 0; i < len; i++) {
-            sum[i + 1] = sum[i] + nums[i];
-        }
-        Map<Integer, Integer> map = new HashMap<>();
-        int count = 0;
-        for (Integer i : sum){
-            int j = i - k;
-            if (map.containsKey(j)) {
-                count += map.get(j);
+        int count=0,pre=0;
+        Map<Integer,Integer> map = new HashMap();
+        map.put(0,1);
+        for (int i = 0; i < nums.length; i++) {
+            pre+=nums[i];
+            if(map.containsKey(pre-k)){
+                count+=map.get(pre-k);
             }
-            map.put(i, map.getOrDefault(i, 0) + 1);
+            map.put(pre,map.getOrDefault(pre,0)+1);
         }
         return count;
     }
