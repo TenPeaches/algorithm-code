15a6451602ad7d1ab895c54b1ea256d479e2834f
TenPeaches <2060853750@qq.com>
2025-11-10 21:25:28 +0800

    leetcode56. 合并区间,法一：排序

--- LeetCode-hot100/src/com/ordinary_array/leetcode56/Method01.java
+++ LeetCode-hot100/src/com/ordinary_array/leetcode56/Method01.java
@@ -6,16 +6,16 @@ import java.util.List;
 
 public class Method01 {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
-        List<int[]> result = new ArrayList<>();
-        for (int[] interval : intervals) {
-            int len = result.size();
-            if(len == 0 || result.get(len-1)[1] < interval[0]){
-                result.add(interval);
+        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+        List<int[]> list=new ArrayList<>();
+        for (int i = 0; i < intervals.length; i++) {
+            int len =list.size();
+            if(len==0||list.get(len-1)[1]<intervals[i][0]){
+                list.add(intervals[i]);
             }else{
-                result.set(len-1, new int[]{result.get(len-1)[0], Math.max(result.get(len-1)[1], interval[1])});
+                list.set(len-1,new int[]{list.get(len-1)[0],Math.max(list.get(len-1)[1],intervals[i][1])});
             }
         }
-        return result.toArray(new int[result.size()][]);
+        return list.toArray(new int[list.size()][2]);
     }
 }
