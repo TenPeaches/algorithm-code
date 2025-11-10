cb5a6590135904287908335a0edd42663b7bbcc3
TenPeaches <2060853750@qq.com>
2025-11-10 22:57:11 +0800

    leetcode73. 矩阵置零,法一：使用标记数组

--- LeetCode-hot100/src/com/matrix/leetcode73/Method01.java
+++ LeetCode-hot100/src/com/matrix/leetcode73/Method01.java
@@ -1,24 +1,21 @@
 package com.matrix.leetcode73;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class Method01 {
     public void setZeroes(int[][] matrix) {
-        Set<Integer> hang = new HashSet<>();
-        Set<Integer> lie = new HashSet<>();
+        int[] hang=new int[matrix.length];
+        int[] lie=new int[matrix[0].length];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
-                if (matrix[i][j] == 0) {
-                    hang.add(i);
-                    lie.add(j);
+                if(matrix[i][j]==0){
+                    hang[i]=1;
+                    lie[j]=1;
                 }
             }
         }
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
-                if (hang.contains(i) || lie.contains(j)) {
-                    matrix[i][j] = 0;
+                if(hang[i]==1||lie[j]==1){
+                    matrix[i][j]=0;
                 }
             }
         }
