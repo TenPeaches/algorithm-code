49deeb187d1ae5230f735270283bb6844a8e7cfc
TenPeaches <2060853750@qq.com>
2025-11-12 21:05:04 +0800

    leetcode101. 对称二叉树,法一：递归

---
+++ LeetCode-hot100/src/com/binary_tree/leetcode101/Method01.java
@@ -0,0 +1,22 @@
+package com.binary_tree.leetcode101;
+
+import com.binary_tree.leetcode94.TreeNode;
+
+public class Method01 {
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null){
+            return true;
+        }
+        return isSymmetric(root.left,root.right);
+    }
+
+    private boolean isSymmetric(TreeNode left,TreeNode right){
+        if(left==null&&right==null){
+            return true;
+        }
+        if(left==null||right==null||left.val!=right.val){
+            return false;
+        }
+        return isSymmetric(left.left,right.right)&&isSymmetric(right.left,left.right);
+    }
+}
