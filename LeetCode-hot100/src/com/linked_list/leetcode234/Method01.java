9941e513b03c1f7603ce9f22e3a241a5f296781d
TenPeaches <2060853750@qq.com>
2025-11-12 10:39:44 +0800

    leetcode234. 回文链表,法一：将值复制到数组中后用双指针法

--- LeetCode-hot100/src/com/linked_list/leetcode234/Method01.java
+++ LeetCode-hot100/src/com/linked_list/leetcode234/Method01.java
@@ -1,21 +1,20 @@
 package com.linked_list.leetcode234;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Method01 {
     public boolean isPalindrome(ListNode head) {
-        List<Integer> list = new LinkedList<>();
-        while (head!= null){
+        List<Integer> list = new ArrayList<>();
+        while (head!=null){
             list.add(head.val);
             head=head.next;
         }
         int i=0,j=list.size()-1;
         while (i<j){
-            if(list.get(i)!=list.get(j))
+            if(list.get(i++)!=list.get(j--)){
                 return false;
-            i++;
-            j--;
+            }
         }
         return true;
     }
