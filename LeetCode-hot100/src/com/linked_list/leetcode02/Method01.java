a5991ae0d74128d42d0b2af1af86b75d46f4e82b
TenPeaches <2060853750@qq.com>
2025-11-12 20:01:49 +0800

    leetcode2. 两数相加,法一：模拟

--- LeetCode-hot100/src/com/linked_list/leetcode02/Method01.java
+++ LeetCode-hot100/src/com/linked_list/leetcode02/Method01.java
@@ -4,29 +4,21 @@ import com.linked_list.leetcode234.ListNode;
 
 public class Method01 {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode head = null, tail = null;
-        int carry = 0;
-        while (l1 != null || l2 != null) {
-            int n1 = l1 != null ? l1.val : 0;
-            int n2 = l2 != null ? l2.val : 0;
-            int sum = n1 + n2 + carry;
-            if (head == null) {
-                head = tail = new ListNode(sum % 10);
-            } else {
-                tail.next = new ListNode(sum % 10);
-                tail = tail.next;
-            }
-            carry = sum / 10;
-            if (l1 != null) {
-                l1 = l1.next;
-            }
-            if (l2 != null) {
-                l2 = l2.next;
-            }
+        ListNode list = new ListNode(0),pre=list;
+        int jinwei=0;
+        while (l1!=null||l2!=null){
+            int shu1=l1!=null?l1.val:0,shu2=l2!=null?l2.val:0;
+            int sum=shu1+shu2+jinwei;
+            int yushu=sum%10;
+            pre.next=new ListNode(yushu);
+            pre=pre.next;
+            jinwei=sum/10;
+            l1=l1!=null?l1.next:l1;
+            l2=l2!=null?l2.next:l2;
         }
-        if (carry > 0) {
-            tail.next = new ListNode(carry);
+        if(jinwei!=0){
+            pre.next=new ListNode(jinwei);
         }
-        return head;
+        return list.next;
     }
 }
