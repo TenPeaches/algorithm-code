6e59bbe1234b3237e3a2afabd5ee10d3c541a821
TenPeaches <2060853750@qq.com>
2025-11-20 16:10:23 +0800

    leetcode200. 岛屿数量,法一：深度优先遍历 DFS

---
+++ LeetCode-hot100/src/com/graph_theory/leetcode200/Mehod01.java
@@ -0,0 +1,27 @@
+package com.graph_theory.leetcode200;
+
+public class Mehod01 {
+    public int numIslands(char[][] grid) {
+        int sum=0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if(grid[i][j]=='1'){
+                    sum++;
+                    dfs(grid,i,j);
+                }
+            }
+        }
+        return sum;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        if(i>=grid.length||i<0||j>=grid[0].length||j<0||grid[i][j]=='0'){
+            return;
+        }
+        grid[i][j]='0';
+        dfs(grid,i-1,j);
+        dfs(grid,i+1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
+    }
+}
