14fa43bf13741d795b8f7274018bd9f995fbab15
TenPeaches <2060853750@qq.com>
2025-12-03 21:13:38 +0800

    leetcode20. 有效的括号,法一：辅助栈法

---
+++ LeetCode-hot100/src/com/stack/leetcode20/Method01.java
@@ -0,0 +1,28 @@
+package com.stack.leetcode20;
+
+import java.util.Stack;
+
+public class Method01 {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if(ch=='('||ch=='{'||ch=='['){
+                stack.push(ch);
+            }else if(ch==')'){
+                if(stack.isEmpty()||stack.pop()!='('){
+                    return false;
+                }
+            }else if(ch=='}'){
+                if(stack.isEmpty()||stack.pop()!='{'){
+                    return false;
+                }
+            }else {
+                if(stack.isEmpty()||stack.pop()!='['){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
