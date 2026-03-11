f2f7dca345d1c767cb1a0ae7f74ae1e86ba6a68a
TenPeaches <15030717+tenpeaches@user.noreply.gitee.com>
2026-03-11 09:56:28 +0800

    leetcode118. 杨辉三角,法一：数学

---
+++ LeetCode-hot100/src/com/dynamic_programming/leetcode118/Method01.java
@@ -0,0 +1,22 @@
+package com.dynamic_programming.leetcode118;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Method01 {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> lists = new ArrayList<>();
+        for (int i=0;i<numRows;i++){
+            List<Integer> list = new ArrayList<>();
+            for (int j = 0; j <=i; j++) {
+                if(j==0||j==i){
+                    list.add(1);
+                }else {
+                    list.add(lists.get(i-1).get(j-1)+lists.get(i-1).get(j));
+                }
+            }
+            lists.add(list);
+        }
+        return lists;
+    }
+}
